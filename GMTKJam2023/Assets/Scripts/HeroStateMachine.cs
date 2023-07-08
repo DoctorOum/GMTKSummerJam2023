b428feb928586ced7345b41e433edaf7de7699d9
Sean Sarmiento <43484827+DoctorOum@users.noreply.github.com>
2023-07-08 00:56:32 -0400

    State switch logic is setup, added nav mesh to move hero. Need animations for hero and actually set up boss
    State switch, logic setup
    Added nav mesh
    ai can move and detect if spot is in an attack or if they are currently standing in an attack

--- GMTKJam2023/Assets/Scripts/HeroStateMachine.cs
+++ GMTKJam2023/Assets/Scripts/HeroStateMachine.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class HeroStateMachine : MonoBehaviour
 {
     private enum HeroStates { Idle, Healing, Attack, Block, Walk, Roll, Run, Dead };
     private HeroStates currentState;
+
+    private NavMeshAgent navAgent;
 
     [SerializeField]
     private int maxHealth;
@@ -19,10 +23,21 @@ public class HeroStateMachine : MonoBehaviour
     private int maxNumOfHealthPotion;
     private int numOfHealthPotion;
 
+    [SerializeField]
+    private int attackCost;
+    [SerializeField]
+    private int rollCost;
+    [SerializeField]
+    private int blockCost;
+
+    private bool tryToHeal = false;
+
     void Start()
     {
         currentState = HeroStates.Idle;
+        navAgent = GetComponent<NavMeshAgent>();
         currentHealth = maxHealth;
+        currentStamina = maxStamina;
         numOfHealthPotion = maxNumOfHealthPotion;
     }
 
@@ -61,73 +76,228 @@ public class HeroStateMachine : MonoBehaviour
         }
     }
 
+    private void CheckAndSetDestination()
+    {
+        if (!navAgent.hasPath)
+        {
+            RaycastHit hit;
+            int randX = Random.Range(-5, 5);
+            int randZ = Random.Range(-5, 5);
+            if (Physics.Raycast(transform.position, new Vector3(transform.forward.x + randX, -.5f, transform.forward.z + randZ), out hit, 25))
+            {
+                if (hit.collider.tag == "AoeAreaObj")
+                {   
+                    Debug.DrawRay(transform.position, new Vector3(transform.forward.x + randX, -.5f, transform.forward.z + randZ) * hit.distance, Color.red);
+                    Debug.Log("Hit aoe area");
+                    //Try to set a new destination if raycast hit a aoe area
+                    CheckAndSetDestination();
+                }
+                else
+                {
+                    Debug.DrawRay(transform.position, new Vector3(transform.forward.x + randX, -.5f, transform.forward.z + randZ) * hit.distance, Color.green);
+                    Debug.Log("Didn't hit aoe area");
+                    navAgent.SetDestination(hit.point);
+                }
+            }
+        }
+        else
+        {
+            return;
+        }
+    }
     private void HeroIdle()
     {
         //If dead do death animation
+        if (currentHealth <= 0)
+        {
+            currentState = HeroStates.Dead;
+        }
 
         //Go to walk
+        currentState = HeroStates.Walk;
     }
 
     private void HeroWalk()
     {
         //Check Health and decide to go towards boss or away
-            //If in range of attack from boss, SWITCH to roll or run, attempt to dodge attack || if Close enough to attack SWITCH to attack
-        
-            //If low health attempt to get away and heal
-                //Check if enough health potion in inventory
-
+        //Health is greater than quarter or if no more health potions left try fighting
+        if (currentHealth > maxHealth * 0.25f || numOfHealthPotion <= 0)
+        {
+            RaycastHit hit;
+            //Checks current floor hero is standing on
+            if (Physics.Raycast(transform.position,Vector3.down, out hit, 25))
+            {
+                //Currently standing in a aoe area
+                if (hit.collider.tag == "AoeAreaObj")
+                {
+                    //Try to roll away
+                    if (currentStamina >= rollCost)
+                    {
+                        currentState = HeroStates.Roll;
+                    }
+                    else if (currentStamina >= blockCost)
+                    {
+                        //Not enough stamina for roll, have hero Block
+                        currentState = HeroStates.Block;
+                    }
+                    //At least 5 percent of stamina left to increase
+                    else if (currentStamina >= maxStamina * .05f)
+                    {
+                        currentState = HeroStates.Run;
+                    }
+                    else
+                    {
+                        //Walk to safe area
+                        CheckAndSetDestination();
+                    }
+                }
+                //Not in AOE Area so either attack if close enough or move towards the boss
+                else
+                {
+                    //Check if in range to make own attack
+                    if (Vector3.Distance(transform.position, BossController.instance.transform.position) < 5 && currentStamina >= attackCost)
+                    {
+                        currentState = HeroStates.Attack;
+                    }
+                    else
+                    {
+                        //Set destination to boss
+                        if (!navAgent.hasPath)
+                        {
+                            navAgent.SetDestination(BossController.instance.transform.position);
+                        }
+                    }
+                }
+            }
+        }
+        //If low health attempt to get away and heal only if hero has potions in inventory
+        else if (currentHealth < maxHealth * 0.25f && numOfHealthPotion > 0)
+        {
+            //If in range of attack, Switch to roll or run, 
+            //Set destination
+            RaycastHit hit;
+            //Checks current floor hero is standing on
+            if (Physics.Raycast(transform.position, Vector3.down, out hit, 25))
+            {
+                //Currently standing in a aoe area
+                if (hit.collider.tag == "AoeAreaObj")
+                {
+                    //Try to roll away
+                    if (currentStamina >= rollCost)
+                    {
+                        currentState = HeroStates.Roll;
+                    }
+                    else if (currentStamina >= blockCost)
+                    {
+                        //Not enough stamina for roll, have hero Block
+                        currentState = HeroStates.Block;
+                    }
+                    //At least 5 percent of stamina left to increase
+                    else if (currentStamina >= maxStamina * .05f)
+                    {
+                        currentState = HeroStates.Run;
+                    }
+                    else
+                    {
+                        //Walk to safe area
+                        CheckAndSetDestination();
+                    }
+                }
+                //Not in AOE Area so either heal
+                else
+                {
+                    currentState = HeroStates.Healing;
+                }
+            }
+        }
+        else if (currentHealth <= 0)
+        {
+            currentState = HeroStates.Dead;
+        }
     }
 
     private void HeroBlock()
     {
-        //Do Block animation
+        //TODO: Do Block animation
+
         //Drain some stamina on impact
+        currentStamina -= blockCost;
 
         //SWITCH to idle when finished
+            //TODO: Make sure to check block animation is done
+        currentState = HeroStates.Idle;
+
     }
 
     private void HeroRoll()
     {
-        //Check Stamina if not enough SWITCH to walk
 
         //pick direction to roll(away from damage most likely)
-        //Do anmiation while moving character in direction
-        //Drain some stamina on use
+        CheckAndSetDestination();
+        //TODO: Do anmiation while moving character in direction
 
-        //SWITCH to idle when finished
+        //Drain some stamina on use
+        currentStamina -= rollCost;
+
+        //SWITCH to Idle when finished
+        if (!navAgent.hasPath)
+        {
+            //TODO: Check if animation is done before SWITCH
+            currentState = HeroStates.Idle;
+        }
     }
 
     private void HeroRun()
     {
-        //If zero stamina SWITCH to walk
+        //Pick where to run and move
+        navAgent.speed = navAgent.speed * 1.5f;
+        CheckAndSetDestination();
+        //TODO: Play run animation
 
-        //Pick where to run
-        //Play run animation
-        //Move hero
         //Drain stamina continously
+        currentStamina -= 1;
 
-        //When done running SWITCH to walk
+        
+        //When done running SWITCH to idle
+        if (!navAgent.hasPath)
+        {
+            currentState = HeroStates.Idle;
+        }
     }
 
     private void HeroAttack()
     {
-        //Check Stamina if not enough SWTICH to walk
+        //TODO: Animation of Swing at the boss
 
-        //Swing at the boss
+        //TODO: Deal damage to boss
+
         //Drain Stamina
+        currentStamina -= attackCost;
 
-        //Animation done SWITCH to walk
+        //Animation done SWITCH to idle
+        currentState = HeroStates.Idle;
     }
 
     private void HeroHeal()
     {
         //Use heal potion
+        numOfHealthPotion -= 1;
+        currentHealth += 50;
 
-        //SWITCH to walk on animation done
+        //SWITCH to idle on animation done
+        currentState = HeroStates.Idle;
     }
 
     private void Death()
     {
-        //Show death animation
+
+        //Double checks if health is at zero
+        if (currentHealth <= 0)
+        {
+            //TODO: Show death animation
+
+            //Calls gamemanagers Hero died
+            GameManager.instance.OnHeroDeath();
+        }
     }
 }
